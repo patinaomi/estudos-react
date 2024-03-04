51c462b73874f9e272475d8e9096b12e9747a864
Patricia Naomi <patricianaomiyama@gmail.com>
2024-03-03 22:38:35 -0300

    inicio docs

---
+++ pomocute/src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Button extends React.Component {
+    render() {
+        return (
+            <button>
+                Button
+            </button>
+        )
+    }
+
+}
+
+export default Button;
---
+++ pomocute/src/components/Form/Form.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Form extends  React.Component {
+    render() {
+        return(
+            <form>
+                <div>
+                <label></label>
+                <input type="text"/>
+                </div>
+                <div>
+                <label></label>
+                <input type="time" name="" id="" />
+                </div>
+            </form>
+        )
+    }
+}
